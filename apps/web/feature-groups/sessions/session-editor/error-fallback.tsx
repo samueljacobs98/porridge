a7e5b0b5d0cb6dc50eb14e30ac59b13975f789dc
Samuel Jacobs <sammyjacobscontact@gmail.com>
2026-03-22 22:11:20 +0100

    feat: add error fallbacks for session-editor and sessions-list

--- apps/web/feature-groups/sessions/session-editor/error-fallback.tsx
+++ apps/web/feature-groups/sessions/session-editor/error-fallback.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { Button } from "@frontend/ui/components/button";
+import { useErrorBoundary } from "react-error-boundary";
+
 export function ErrorFallback() {
-  return <div>Error</div>;
+  const { resetBoundary } = useErrorBoundary();
+
+  return (
+    <div className="flex flex-col gap-0.5">
+      <h1 className="text-2xl font-semibold">Failed to Load Session</h1>
+      <p className="text-sm">
+        An error occurred while loading the session. Try again later.
+      </p>
+      <Button onClick={() => resetBoundary()} className="mt-4 w-fit">
+        Retry
+      </Button>
+    </div>
+  );
 }
--- apps/web/feature-groups/sessions/sessions-list/error-fallback.tsx
+++ apps/web/feature-groups/sessions/sessions-list/error-fallback.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { Button } from "@frontend/ui/components/button";
+import { useErrorBoundary } from "react-error-boundary";
+
 export function ErrorFallback() {
-  return <div>Error</div>;
+  const { resetBoundary } = useErrorBoundary();
+
+  return (
+    <div className="flex flex-col gap-0.5 p-2">
+      <div className="flex w-fit flex-col items-center justify-center gap-2 rounded-md border bg-muted/50 px-6 py-4">
+        <p className="text-sm">
+          An error occurred while loading the session. Try again later.
+        </p>
+        <Button onClick={() => resetBoundary()} className="mx-auto w-fit">
+          Retry
+        </Button>
+      </div>
+    </div>
+  );
 }
